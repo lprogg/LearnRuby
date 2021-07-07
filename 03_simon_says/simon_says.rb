f78ea9ce46ae716b4d3a17de1b36df55f7089179
lprogg <lprogg>
2021-07-07 18:55:51 +0300

    Update code

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,29 +6,27 @@ def shout(str)
   str.upcase
 end
 
-def repeat(str = "hello", n = 2)
-  result = ""
-  (0...n).each { result += str + " " }
-  result.strip
+def repeat(str = 'hello', n = 2)
+  (0...n).map { str }.join(' ').strip
 end
 
-def start_of_word(str = "", n = 1)
+def start_of_word(str = '', n = 1)
   str[0...n]
 end
 
 def first_word(str)
-  str.partition(" ").first
+  str.partition(' ').first
 end
 
 def titleize(str)
   str.capitalize!
   words_no_cap = %w[and over the]
 
-  result = str.split(" ").map { |item|
+  str.split.map do |item|
     if words_no_cap.include?(item)
       item
     else
       item.capitalize
     end
-  }.join(" ")
+  end.join(' ')
 end
