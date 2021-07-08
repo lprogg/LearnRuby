849b04cb63f04eb312dab3b4ca294532840e7806
lprogg <lprogg>
2021-07-08 13:06:56 +0300

    Add solution to exercise 10

---
+++ 10_temperature_object/temperature.rb
@@ -0,0 +1,49 @@
+class Temperature
+  def initialize(temp_hash = {})
+    @temp_hash = temp_hash
+  end
+
+  def self.from_fahrenheit(arg)
+    Temperature.new({ f: arg })
+  end
+
+  def self.from_celsius(arg)
+    Temperature.new({ c: arg })
+  end
+
+  def self.ftoc(arg)
+    (5.0 / 9.0) * (arg.to_f - 32.0)
+  end
+
+  def self.ctof(arg)
+    ((9.0 / 5.0) * arg.to_f) + 32.0
+  end
+
+  def in_celsius
+    if @temp_hash.key?(:c)
+      @temp_hash[:c]
+    else
+      Temperature.ftoc(@temp_hash[:f])
+    end
+  end
+
+  def in_fahrenheit
+    if @temp_hash.key?(:f)
+      @temp_hash[:f]
+    else
+      Temperature.ctof(@temp_hash[:c])
+    end
+  end
+end
+
+class Celsius < Temperature
+  def initialize(arg)
+    super({c: arg})
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(arg)
+    super({f: arg})
+  end
+end
