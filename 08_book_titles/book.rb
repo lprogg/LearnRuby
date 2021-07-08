27d39021fa1cbb4385397bb43195c5f90401184d
lprogg <lprogg>
2021-07-08 17:56:25 +0300

    Refactor exercise 08

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -6,6 +6,6 @@ class Book
   end
 
   def capitalize_title(str)
-    str.capitalize!.split.map { |item| %w[and in the of a an].include?(item) ? item : item.capitalize }.join(' ')
+    str.capitalize.split.map { |item| %w[and in the of a an].include?(item) ? item : item.capitalize }.join(' ')
   end
 end
