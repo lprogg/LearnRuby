4e6fdaf8752619df11ec94ef5b15a8a5b00b3086
lprogg <lprogg>
2021-07-11 11:58:43 +0300

    Add minus method

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -17,8 +17,22 @@ class RPNCalculator
       @total += @stack.last(2).sum
       @stack.pop(2)
       @total
-    elsif @stack.length == 1
+    elsif @stack.length.equal?(1)
       @total += @stack[0]
+      @stack.pop(1)
+      @total
+    elsif @stack.empty?
+      raise 'calculator is empty'
+    end
+  end
+
+  def minus
+    if @stack.length >= 2
+      @total += (@stack.last(2)[0] - @stack.last(2)[1])
+      @stack.pop(2)
+      @total
+    elsif @stack.length.equal?(1)
+      @total -= @stack[1]
       @stack.pop(1)
       @total
     elsif @stack.empty?
